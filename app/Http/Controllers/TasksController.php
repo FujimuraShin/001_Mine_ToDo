31645a58915a71100b5472dd79dff30a9d086622
SF <sfujimura2@gmail.com>
2023-08-06 16:45:45 +0900

    004_Routing_ViewIndexFile

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TasksController extends Controller
 {
     //
+    public function index(){
+
+        $tasks=Task::get();
+        return view('tasks.index',compact('tasks'));
+    }
 }
